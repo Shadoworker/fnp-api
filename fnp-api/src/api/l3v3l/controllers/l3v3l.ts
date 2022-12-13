739cdad1c703fc01d999801fbf6cb43e240eeb2e
habibeshadow <habibe@kayfo.games>
2022-12-13 23:42:12 +0000

    - Formatage de l'objet poisson retourné - Get des ressources d'un jeu - Update d'une ressource d'un joueur

--- fnp-api/src/api/l3v3l/controllers/l3v3l.ts
+++ fnp-api/src/api/l3v3l/controllers/l3v3l.ts
@@ -33,6 +33,7 @@ export default factories.createCoreController('api::l3v3l.l3v3l', ({ strapi }) =
 
         const auth_header = btoa(`${client_id}:${client_secret}`);
 
+        // console.log(auth_header)
 
         let config = {
           headers: {
@@ -64,7 +65,7 @@ export default factories.createCoreController('api::l3v3l.l3v3l', ({ strapi }) =
               // console.log(jwt)
               // Return values
               var claims = JSON.stringify(jwt.claims);
-              console.log(claims)
+              // console.log(claims)
 
               ctx.body = view(accessTokenString, claims);
   
@@ -103,7 +104,7 @@ export default factories.createCoreController('api::l3v3l.l3v3l', ({ strapi }) =
             // console.log(jwt)
             // Return values
             var claims = JSON.stringify(jwt.claims);
-            console.log(claims)
+            // console.log(claims)
 
             ctx.body = claims;
 
@@ -132,8 +133,6 @@ export default factories.createCoreController('api::l3v3l.l3v3l', ({ strapi }) =
           }
          }
 
-        var body = null;
-        
         // Get Token
         try {
 
@@ -142,6 +141,8 @@ export default factories.createCoreController('api::l3v3l.l3v3l', ({ strapi }) =
 
           var accessTokenString = data.access_token;
  
+          // console.log(accessTokenString)
+
           // ctx.body = accessTokenString;
           // Get games ...
           let config2 = {
@@ -176,8 +177,75 @@ export default factories.createCoreController('api::l3v3l.l3v3l', ({ strapi }) =
       },
 
 
+ 
+
+      async getGameRessources(ctx) { // l3v3l: get game resources list
+
+        var url_string = CONSTANTS.api_url+ ctx.originalUrl;
+
+        var url = new URL(url_string);
+
+        var gameId = JSON.parse(url.searchParams.get("gameId"));
+
+        let client_id = CONSTANTS.requests_client_id;
+        let client_secret = CONSTANTS.requests_client_secret;
+
+        const auth_header = btoa(`${client_id}:${client_secret}`);
+
+        let config = {
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/x-www-form-urlencoded',
+            Authorization: 'Basic '+ auth_header
+          }
+         }
+
+        
+        // Get Token
+        try {
+
+          let _url = CONSTANTS.playtix_request_base_url+'/v1/token?grant_type=client_credentials';
+          const { data } = await axios.post(_url, undefined, config)
+
+          var accessTokenString = data.access_token;
+ 
+          console.log(accessTokenString)
+
+          // ctx.body = accessTokenString;
+          // Get games ...
+          let config2 = {
+            headers: {
+              'Accept-Encoding' : 'application/json',
+              'Accept': 'application/json',
+              'Content-Type': 'application/json;charset=utf-8',
+              Authorization: 'Bearer '+ accessTokenString
+            }
+           }
+          try {
+
+            let _url2 = CONSTANTS.playtix_api_base_url+'resources/game/'+gameId;
+            
+            const { data } = await axios.post(_url2, undefined, config2);
+
+            console.log(data);
+            ctx.body = data;
+              
+  
+          } catch (err) {
+            ctx.body = err;
+          }
+         
+
+        } catch (err) {
+          ctx.body = err;
+        }
+ 
+
+
+      },
+
       
-      async getAFish(ctx) { // get a random fish : with sent specs
+      async getFish(ctx) { // get a random fish : with sent specs
 
             
           var url_string = CONSTANTS.api_url+ ctx.originalUrl;
@@ -197,11 +265,12 @@ export default factories.createCoreController('api::l3v3l.l3v3l', ({ strapi }) =
             {
             },
             populate: {
-              espece: {
+              species: {
                 populate: {
                   id_mapping: true,
                 },
               },
+              fish_sizes:true
             }
           }
         
@@ -219,11 +288,12 @@ export default factories.createCoreController('api::l3v3l.l3v3l', ({ strapi }) =
               {
               },
               populate: {
-                espece: {
+                species: {
                   populate: {
                     id_mapping: true,
                   },
                 },
+                fish_sizes:true
               }
             }
             const entity = await strapi.service('api::fish.fish').find(query);
@@ -236,17 +306,108 @@ export default factories.createCoreController('api::l3v3l.l3v3l', ({ strapi }) =
         var selected_fish = fishes[r];
 
           // Getting fish sizes
-        var sizes = strapi.getModel('api::fish.fish').attributes.size.enum;
+        var sizes = selected_fish["fish_sizes"];
         var r_size = Math.floor(Math.random() * sizes.length);
-        var size = sizes[r_size];
+        var size = sizes[r_size]["name"];
+        var size_ratio = sizes[r_size]["ratio"];
 
-        selected_fish["size"] = size;
+        var fish_weight = selected_fish["species"]["base_weight"] * size_ratio;
 
 
-        ctx.body = selected_fish;
+        let returnedFish = {
+          name : selected_fish["species"]["name"],
+          size : size,
+          weight: fish_weight
+        }
+
+        ctx.body = returnedFish;
 
 
       },
+
+
+
+      async updatePlayerResource(ctx) { // l3v3l: update a specific resource for a player
+
+        var url_string = CONSTANTS.api_url+ ctx.originalUrl;
+
+        var url = new URL(url_string);
+
+        // Params
+        var playerId = JSON.parse(url.searchParams.get("playerId"));
+        var resourceId = JSON.parse(url.searchParams.get("resourceId"));
+        var resourceAmount = JSON.parse(url.searchParams.get("resourceAmount"));
+        // ----------------------
+
+        let client_id = CONSTANTS.requests_client_id;
+        let client_secret = CONSTANTS.requests_client_secret;
+
+        const auth_header = btoa(`${client_id}:${client_secret}`);
+
+        let config = {
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/x-www-form-urlencoded',
+            Authorization: 'Basic '+ auth_header
+          }
+         }
+
+        
+        // Get Token
+        try {
+
+          let _url = CONSTANTS.playtix_request_base_url+'/v1/token?grant_type=client_credentials';
+          const { data } = await axios.post(_url, undefined, config)
+
+          var accessTokenString = data.access_token;
+ 
+          // console.log(accessTokenString)
+
+          // ctx.body = accessTokenString;
+          // Get games ...
+          let config2 = {
+            headers: {
+              'Accept-Encoding' : 'application/json',
+              'Accept': 'application/json',
+              'Content-Type': 'application/json;charset=utf-8',
+              Authorization: 'Bearer '+ accessTokenString
+            }
+           }
+
+          
+          let _body = {
+            player_id: playerId,
+            resources: [
+              {
+                resource_id: resourceId,
+                amount: resourceAmount
+              }
+            ]
+          }
+
+          try {
+
+            let _url2 = CONSTANTS.playtix_api_base_url+'player-resources';
+            
+            const { data } = await axios.post(_url2, _body, config2);
+
+            // console.log(data);
+            ctx.body = data;
+              
+  
+          } catch (err) {
+            ctx.body = err;
+          }
+         
+
+        } catch (err) {
+          ctx.body = err;
+        }
+ 
+
+
+      },
+
 
 
   }));
